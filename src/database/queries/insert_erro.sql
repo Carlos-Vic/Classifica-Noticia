7f471414f07260eec512125cc3124ae9b3946402
Carlos Victor <carlosvictoralbuquerque221@gmail.com>
2026-04-29 18:20:04 -0300

    refatorando query de insert erro para ignorar conflito de url

--- src/database/queries/insert_erro.sql
+++ src/database/queries/insert_erro.sql
@@ -1,3 +1,3 @@
 INSERT INTO erros (url, label, erro, dataColeta)
 VALUES (%s,%s,%s,%s)
-ON CONFLICT (url) DO NOTHING
+ON CONFLICT (url) DO UPDATE SET erro = EXCLUDED.erro, dataColeta = EXCLUDED.dataColeta
